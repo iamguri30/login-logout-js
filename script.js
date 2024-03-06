07b188df50718b8f48cd210c3a35722be8b3a1ec
iamguri30 <149175281+iamguri30@users.noreply.github.com>
2024-03-06 09:28:01 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var username = localStorage.getItem("username");
-// console.log(username);
 
 function checkuser() {
   if (username) {
@@ -16,28 +15,33 @@ checkuser();
 function sendData() {
   var getuser = document.getElementById("user").value;
   var getpwd = document.getElementById("pwd").value;
-
+  
   if (!localStorage.getItem("username")) {
-    localStorage.setItem("username", getuser);
-    username = getuser; // Update the username variable
-    document.getElementById("login").style.display = "none";
-    document.getElementById("logout").style.display = "block";
-    alert("Login successful");
-  }
+      localStorage.setItem("username", getuser);
+      username = getuser; // Update the username variable
+      document.getElementById("login").style.display = "none";
+      document.getElementById("logout").style.display = "block";
+      alert("Login successful");
+
+      var getuser = document.getElementById("user").value='';
+      var getpwd = document.getElementById("pwd").value='';
+
+    }
 }
 
 // logout user
 function logout() {
-  localStorage.removeItem("username");
-  document.getElementById("logout").style.display = "none";
-  document.getElementById("login").style.display = "block";
+    localStorage.removeItem("username");
+    document.getElementById("logout").style.display = "none";
+    document.getElementById("login").style.display = "block";
+    // username = '';
   alert("Logout");
 }
 
 // add to cart
 function addtocart() {
   console.log(username);
-  if (username == null) {
+  if (!localStorage.getItem("username")) {
     alert("login first");
   } else {
     alert("product added to cart");
